8b2d38675c4be9994998a11cb3886ec3848f951e
Nico Tonozzi <dominic.tonozzi@gmail.com>
2014-10-13 17:54:14 -0600

    headerfile

--- two/multi-lookup.h
+++ two/multi-lookup.h
@@ -1,7 +1,2 @@
-/* Fuction to return the first IP address found
- * for hostname. IP address returned as string
- * firstIPstr of size maxsize
- */
-int dnslookup(const char* hostname,
-	      char* firstIPstr,
-	      int maxSize);
+void request(char *input[]);
+void resolve(char *output[]);
