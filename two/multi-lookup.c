9e591047c5d4137e1ac04440c4cf46fbba24e10e
Nico Tonozzi <dominic.tonozzi@gmail.com>
2014-10-13 20:13:40 -0600

    Properly passing arguments into threaded functions

--- two/multi-lookup.c
+++ two/multi-lookup.c
@@ -5,18 +5,25 @@
 
 #define THREAD_MAX 10
 
-pthread_mutex_t file = PTHREAD_MUTEX_INITALIZER;
-pthread_mutex_t queue = PTHREAD_MUTEX_INITALIZER;
+pthread_mutex_t queue = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t file  = PTHREAD_MUTEX_INITIALIZER;
 
-void request(char input[])
+typedef struct {
+    char *fileName;
+} threadParam;
+
+void* request(void* param)
 {
     // Read the file
-
     FILE *file;
     char *line = NULL;
     size_t len = 0;
     char read;
-    file=fopen(input, "r");
+    char *fileName;
+    threadParam *param2 = param;
+    fileName = param2->fileName;
+    printf("%s", fileName);
+    file = fopen(fileName, "r");
 
     if (file == NULL){
         printf("ERROR: File is Empty");
@@ -29,12 +36,13 @@ void request(char input[])
 
     if (line) {
         free(line);
-    }  
+    }
 
     // for each line
     // Lock the queue
     // Put the name on the queue
     // Unlock the queue
+    return NULL;
 }
 
 void resolve(char *output[])
@@ -52,10 +60,27 @@ void resolve(char *output[])
 
 int main(int argc, char *argv[])
 {
-    // Call the requester for each fill
+    // Call the requester for each file
+    int i;
+    int threadCount = argc-1;
+    pthread_t threads[threadCount];
+    int err;
+    threadParam param[threadCount];
 
-    request(argv[1]);
+    for (i = 0; i < threadCount; i++) {
+        param[i].fileName = argv[i+1];
+        err = pthread_create(&(threads[i]), NULL, request, &param[i]);
+        if (err){
+            printf("ERROR on pthread create: %d\n", err);
+        }
+    }
+    
+    //Waiting for threads to finish
+    for(i = 0; i<threadCount; i++){
+        pthread_join(threads[i], NULL);
+    }
+
     // Spawn THEAD_MAX threads in resolve
-
+    
     return 0;
 }
