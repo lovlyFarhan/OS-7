cb5f30fb4b3b8971cfd3572ac0b791e61f511265
Jacob Resman <jacob.resman@colorado.edu>
2014-10-13 18:05:22 -0600

    File Reading

--- two/multi-lookup.c
+++ two/multi-lookup.c
@@ -6,32 +6,54 @@
 #define THREAD_MAX 10
 
 pthread_mutex queue = PTHREAD_MUTEX_INITALIZER;
-pthread_mutex file= PTHREAD_MUTEX_INITALIZER;
+pthread_mutex file = PTHREAD_MUTEX_INITALIZER;
 
 void request(char *input[])
 {
-	// Read the file
-	// for each line
-	// Lock the queue
-	// Put the name on the queue
-	// Unlock the queue
+    // Read the file
+
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    char read;
+    file=fopen(input, "r");
+
+    if (file == NULL){
+        printf("ERROR: File is Empty");
+    }  
+
+    while ((read = getline(&line, &len, file)) != -1) {
+        printf("Retrieved line of length %d :\n", read);
+        printf("%s", line);
+    }
+
+    if (line) {
+        free(line);
+    }  
+
+    // for each line
+    // Lock the queue
+    // Put the name on the queue
+    // Unlock the queue
 }
 
 void resolve(char *output[])
 {
-	// while not locked
-	// Lock the queue
-	// If there's no more items in the queue, return
-	// Take the domain off the queue
-	// Unlock the queue
-	// get an IP
-	// lock the file
-	// write the comma and IP
-	// unlock the file
+    // while not locked
+    // Lock the queue
+    // If there's no more items in the queue, return
+    // Take the domain off the queue
+    // Unlock the queue
+    // get an IP
+    // lock the file
+    // write the comma and IP
+    // unlock the file
 }
 
 int main(int argc, char *argv[])
 {
-	// Call the requester for each file
-	// Spawn THEAD_MAX threads in resolve
+    // Call the requester for each fill
+
+    request(argv[1]);
+    // Spawn THEAD_MAX threads in resolve
 }
