1971d591deee26752021e4ce03d5f0b06cee174e
Nico Tonozzi <dominic.tonozzi@gmail.com>
2014-11-02 20:31:46 -0700

    print command before time

--- three/test.sh
+++ three/test.sh
@@ -8,7 +8,7 @@ SCHEDULER=( SCHED_OTHER SCHED_FIFO SCHED_RR )
 for p in ${PROGRAM[@]}; do
     for s in ${SCHEDULER[@]}; do
         for t in ${THREADS[@]}; do
-            sudo /usr/bin/time -f "$TIMEFORMAT" ./$p $s $t > out
+            echo $p $s $t; sudo /usr/bin/time -f "$TIMEFORMAT" ./$p $s $t > out
         done
     done
 done
